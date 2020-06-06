a8943041ae26c290adeca9cc80ec487b999c5215
DaniDush <55020509+DaniDush@users.noreply.github.com>
2020-06-06 21:54:30 +0300

    Still without database

---
+++ server/src/main/java/il/cshaifasweng/HSTS/server/UserController.java
@@ -0,0 +1,17 @@
+package il.cshaifasweng.HSTS.server;
+
+import java.io.IOException;
+
+
+public class UserController {
+	
+	
+	public static int getRole(String userName, String pass) {
+		//TODO role pick and checks
+		return 1;
+		
+	}
+	
+	
+
+}
