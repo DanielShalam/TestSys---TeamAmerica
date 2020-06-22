8b8032ff1dafd163c60bf7fd77408991a91d84cf
DaniDush <55020509+DaniDush@users.noreply.github.com>
2020-06-23 00:08:13 +0300

    Somthing

--- client/src/main/java/il/cshaifasweng/HSTS/client/StudentMenuController.java
+++ client/src/main/java/il/cshaifasweng/HSTS/client/StudentMenuController.java
@@ -1,61 +1,73 @@
-
 package il.cshaifasweng.HSTS.client;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class StudentMenuController {
 
-    @FXML // fx:id="startExamButton"
-    private Button startExamButton; // Value injected by FXMLLoader
+    @FXML
+    private AnchorPane execTF;
 
-    @FXML // fx:id="viewGradesButton"
-    private Button viewGradesButton; // Value injected by FXMLLoader
+    @FXML
+    private Button startExamButton;
 
-    @FXML // fx:id="viewPerformedExamsButton"
-    private Button viewPerformedExamsButton; // Value injected by FXMLLoader
+    @FXML
+    private Button viewGradesButton;
 
-    @FXML // fx:id="viewGradesTV"
-    private TableView<?> viewGradesTV; // Value injected by FXMLLoader
+    @FXML
+    private Button viewPerformedExamsButton;
 
-    @FXML // fx:id="subjectTC"
-    private TableColumn<?, ?> subjectTC; // Value injected by FXMLLoader
+    @FXML
+    private TableView<?> viewGradesTV;
 
-    @FXML // fx:id="courseTC"
-    private TableColumn<?, ?> courseTC; // Value injected by FXMLLoader
+    @FXML
+    private TableColumn<?, ?> subjectTC;
 
-    @FXML // fx:id="dateTC"
-    private TableColumn<?, ?> dateTC; // Value injected by FXMLLoader
+    @FXML
+    private TableColumn<?, ?> courseTC;
 
-    @FXML // fx:id="gradeTC"
-    private TableColumn<?, ?> gradeTC; // Value injected by FXMLLoader
+    @FXML
+    private TableColumn<?, ?> dateTC;
 
-    @FXML // fx:id="examCopyTV"
-    private TableView<?> examCopyTV; // Value injected by FXMLLoader
+    @FXML
+    private TableColumn<?, ?> gradeTC;
+
+    @FXML
+    private TableView<?> examCopyTV;
+
+    @FXML
+    private TextField execCodeTF;
+
+    @FXML
+    private TableView<?> studentExamsTV;
+
+    @FXML
+    private TableColumn<?, ?> instCourseTC;
+    
+    @FXML
+    private TableColumn<?, ?> instTeacherTC;
+    
+    @FXML
+    private TableColumn<?, ?> instDateTC;
 
     @FXML
     void createStudentExamPageBoundary(ActionEvent event) {
-    	
-    	//App.setRoot("StudentExamPage");
+
     }
 
     @FXML
     void getGrades(ActionEvent event) {
-    	//getGradesByStudent(student_ID); TODO - implement in Client Data Controller
-    	// TODO - implement loading of data from getGradesByStudent to viewGradesTV
-    	viewGradesTV.setVisible(true);
-    	viewGradesTV.toFront();
+
     }
 
     @FXML
     void viewExams(ActionEvent event) {
-    	//getExamCopy(student_ID); TODO - implement in Client Data Controller
-    	// TODO - implement loading of data from getExamCopy to examCopyTV
-    	examCopyTV.setVisible(true);
-    	examCopyTV.toFront();
+
     }
-    
+
 }
